67f6ece88b9b5140650d57383353892ae4d4a976
MDac10 <76761948+MDac10@users.noreply.github.com>
2022-03-24 17:23:50 -0400

    Create headers.h

---
+++ CS2211/Assignments/asn4/headers.h
@@ -0,0 +1,12 @@
+/*CS2211a 2020*/
+/*Assignment 04*/
+/*Megan Da Costa*/
+/*mdacos2*/
+/*November 16 2020*/
+
+#include <stdio.h>
+#include <string.h>
+#include "definitions.h"
+
+#include "inputFunctions.c"
+#include "outputFunctions.c"
